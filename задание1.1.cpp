26ac73c6a1461121c6088dc5d1fa72f4871d4269
akenohimeji <holodvada@gmail.com>
2026-05-21 10:16:19 +0700

    Добавил два задания

---
+++ задание1.1.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+class Printable {
+public:
+    virtual ~Printable() = default;
+    virtual void render(const std::string& text) const = 0;
+};
+
+class Text : public Printable {
+public:
+    void render(const std::string& text) const override {
+        std::cout << text;
+    }
+};
+
+class Decorator : public Printable {
+protected:
+    Printable* component;
+public:
+    Decorator(Printable* comp) : component(comp) {}
+    virtual ~Decorator() { delete component; }
+};
+
+class Paragraph : public Decorator {
+public:
+    Paragraph(Printable* comp) : Decorator(comp) {}
+
+    void render(const std::string& text) const override {
+        std::cout << "<p>";
+        component->render(text);
+        std::cout << "</p>";
+    }
+};
+
+class Reversed : public Decorator {
+public:
+    Reversed(Printable* comp) : Decorator(comp) {}
+
+    void render(const std::string& text) const override {
+        std::string reversed_text = text;
+        std::reverse(reversed_text.begin(), reversed_text.end());
+        component->render(reversed_text);
+    }
+};
+
+class Link : public Decorator {
+public:
+    Link(Printable* comp) : Decorator(comp) {}
+
+    void render(const std::string& text) const override {
+        std::cout << "<a href=\x22\x22>";
+        component->render(text);
+        std::cout << "</a>";
+    }
+
+    void render(const std::string& href, const std::string& text) const {
+        std::cout << "<a href=" << href << ">";
+        component->render(text);
+        std::cout << "</a>";
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    auto text_block_p = new Paragraph(new Text());
+    text_block_p->render("Hello world");
+    std::cout << "\n\n";
+
+    auto text_block_r = new Reversed(new Text());
+    text_block_r->render("Hello world");
+    std::cout << "\n\n";
+
+    // Тест 3: Link
+    auto text_block_l = new Link(new Text());
+    text_block_l->render("netology.ru", "Hello world");
+    std::cout << "\n";
+
+    return 0;
+}
---
+++ задание2.2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <memory>
+
+class VeryHeavyDatabase {
+public:
+    virtual ~VeryHeavyDatabase() = default;
+
+    virtual std::string GetData(const std::string& key) const {
+        return "value"; 
+    }
+};
+
+class OneShotDB : public VeryHeavyDatabase {
+private:
+    VeryHeavyDatabase* real_object; 
+    mutable size_t shots;           
+
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1)
+        : real_object(real_object), shots(shots) {
+    }
+
+
+    std::string GetData(const std::string& key) const override {
+        if (shots > 0) {
+            --shots; 
+            return real_object->GetData(key); 
+        }
+
+        return "error"; 
+    }
+};
+
+int main() {
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+
+    std::cout << limit_db.GetData("key") << std::endl; 
+    std::cout << limit_db.GetData("key") << std::endl; 
+    std::cout << limit_db.GetData("key") << std::endl; 
+
+    return 0;
+}
